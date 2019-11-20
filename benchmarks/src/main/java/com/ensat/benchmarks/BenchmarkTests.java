9eefe621ee68471d90aaaf37dfd3c6f14325ad80
losete <losetazo@gmail.com>
2019-11-20 14:09:08 +0100

    Divided in two modules

--- benchmarks/src/main/java/com/ensat/benchmarks/BenchmarkTests.java
+++ benchmarks/src/main/java/com/ensat/benchmarks/BenchmarkTests.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.ensat.SpringBootWebApplication;
 import java.util.concurrent.TimeUnit;
 import org.junit.runner.RunWith;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -14,9 +16,7 @@ public class BenchmarkTests {
 
   @Benchmark
   @OutputTimeUnit(TimeUnit.MILLISECONDS)
-  @RunWith(SpringJUnit4ClassRunner.class)
-  @SpringBootTest(classes = SpringBootWebApplication.class)
-  public void benchmarkLoadContext{
+  public void benchmarkLoadContext(){
 
   }
 
