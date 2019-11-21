ede2760c663b38b3b48b6fffef7bcef301679776
losete <losetazo@gmail.com>
2019-11-21 09:09:17 +0100

    Shade plugin configured

--- benchmarks/src/main/java/com/ensat/benchmarks/BenchmarkTests.java
+++ benchmarks/src/main/java/com/ensat/benchmarks/BenchmarkTests.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.ensat.SpringBootWebApplication;
 import java.util.concurrent.TimeUnit;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
@@ -12,11 +13,16 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = SpringBootWebApplication.class)
+@WebAppConfiguration
 public class BenchmarkTests {
 
   @Benchmark
   @OutputTimeUnit(TimeUnit.MILLISECONDS)
-  public void benchmarkLoadContext(){
+  @Test
+  public void contextLoads(){
+
 
   }
 
